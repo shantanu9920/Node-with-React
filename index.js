31618065939300bb0032824291f4dc7d4b630dd0
shantanu9920 <shantanu9920kumar@gmail.com>
2020-05-12 19:48:36 +0530

    Domain Verification Changes

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys');
+// const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
  
-require('./models/user'); 
-require('./services/passport');
+// require('./models/user'); 
+// require('./services/passport');
 
-mongoose.connect(keys.mongoURI,  { useNewUrlParser: true });
+// mongoose.connect(keys.mongoURI,  { useNewUrlParser: true });
 const app = express();
 
-app.use(
-    cookieSession({
-        maxAge: 30*24*60*60*1000,
-        keys: [keys.cookieKey]
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//     cookieSession({
+//         maxAge: 30*24*60*60*1000,
+//         keys: [keys.cookieKey]
+//     })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 authRoutes(app);
 
